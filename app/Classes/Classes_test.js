19a8a324b982bff478ebdbddac821e2bd7ecee7a
Rohaan Advani <RohaanAdvani68@gmail.com>
2017-05-08 03:45:32 -0500

    Have basic my classes page set up- need to figure out angular routing

---
+++ app/Classes/Classes_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.Classes module', function() {
+
+  beforeEach(module('myApp.Classes'));
+
+  describe('Classes controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var ClassesCtrl = $controller('ClassesCtrl');
+      expect(ClassesCtrl).toBeDefined();
+    }));
+
+  });
+});
